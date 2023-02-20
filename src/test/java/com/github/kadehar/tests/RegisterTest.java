c8475bfc591b67cd121ce94f164f6ee537849f9a
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2023-02-20 12:15:47 +0300

    add faker

--- src/test/java/com/github/kadehar/tests/RegisterTest.java
+++ src/test/java/com/github/kadehar/tests/RegisterTest.java
@@ -1,8 +1,8 @@
 package com.github.kadehar.tests;
 
+import com.github.javafaker.Faker;
 import com.github.kadehar.client.RegisterClient;
 import com.github.kadehar.model.User;
-import com.github.kadehar.steps.RegisterSteps;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +11,15 @@ import java.util.Map;
 public class RegisterTest extends TestBase {
     private final User user = new User();
     private final RegisterClient client = new RegisterClient();
+    private final Faker faker = new Faker();
 
     @Test
     public void registerUser() {
-        user.setFirstName("Test")
-                .setLastName("Test")
-                .setEmail("nidoyef352@charav.com")
-                .setPassword("111111")
-                .setGender("M");
+        user.setFirstName(faker.name().firstName())
+                .setLastName(faker.name().lastName())
+                .setEmail(faker.internet().emailAddress())
+                .setPassword(faker.internet().password(6, 14, true))
+                .setGender(faker.demographic().sex());
 
         Response register = client.register();
         String token = register.htmlPath().getString("**.find{it.@name == '__RequestVerificationToken'}.@value");
